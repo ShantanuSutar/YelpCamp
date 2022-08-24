b3fff2165af3f2b62168b44475f3c876d963b6ef
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-08-24 16:18:49 +0530

    Campground Index

--- app.js
+++ app.js
@@ -21,12 +21,11 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({ title: 'My Backyard', description: 'cheap camping' });
-    await camp.save();
-    res.send(camp)
-
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({})
+    res.render('campgrounds/index', { campgrounds })
 })
+
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
