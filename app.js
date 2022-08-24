695fffdfc37c2b052e8fd50d09da88c269e82db3
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-08-24 22:35:25 +0530

    Campground Delete

--- app.js
+++ app.js
@@ -56,6 +56,13 @@ app.put('/campgrounds/:id', async (req, res) => {
 
 })
 
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+})
+
+
 app.listen(3000, () => {
     console.log('Serving on port 3000')
 })
