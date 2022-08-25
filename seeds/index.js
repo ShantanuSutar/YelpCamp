1709bf9386b630614b0c067862d8371af5c9a6f9
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-08-25 13:24:07 +0530

    Adding Images

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,13 @@ const seedDB = async () => {
 
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui repellat cum, facere voluptate fugit tempore blanditiis obcaecati nobis, omnis mollitia a amet magnam exercitationem non voluptatem quas incidunt veritatis quia!',
+            price
         })
         await camp.save();
     }
@@ -28,4 +32,5 @@ const seedDB = async () => {
 
 seedDB().then(() => {
     mongoose.connection.close();
-}); 
+});
+
