272a9ed99b6b7c50c40d4e69e042ee45ee2b1e50
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-08-26 11:56:02 +0530

    Basic Error Handler

--- app.js
+++ app.js
@@ -35,10 +35,14 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 })
 
-app.post('/campgrounds', async (req, res) => {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)
+app.post('/campgrounds', async (req, res, next) => {
+    try {
+        const campground = new Campground(req.body.campground);
+        await campground.save();
+        res.redirect(`/campgrounds/${campground._id}`)
+    } catch (e) {
+        next(e);
+    }
 })
 
 app.get('/campgrounds/:id', async (req, res) => {
@@ -64,6 +68,9 @@ app.delete('/campgrounds/:id', async (req, res) => {
     res.redirect('/campgrounds');
 })
 
+app.use((err, req, res, next) => {
+    res.send("OH BOY! Something went wrong!")
+})
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
