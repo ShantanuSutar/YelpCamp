2c2797540f34769f92375504cac1a856d5a7315f
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-08-30 12:07:53 +0530

    Validating Reviews

--- schemas.js
+++ schemas.js
@@ -1,3 +1,4 @@
+const { number } = require('joi');
 const Joi = require('joi');
 
 module.exports.campgroundSchema = Joi.object({
@@ -9,3 +10,10 @@ module.exports.campgroundSchema = Joi.object({
         description: Joi.string().required(),
     }).required()
 });
+
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().required().min(1).max(5),
+        body: Joi.string().required()
+    }).required()
+})
