994f515c130660b31d7668a022843538b74a37ba
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-08-31 23:44:13 +0530

    Setting Up Flash

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const session = require('express-session');
+const flash = require('connect-flash')
 const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override')
 
 const campgrounds = require('./routes/campgrounds')
-const reviews = require('./routes/reviews')
+const reviews = require('./routes/reviews');
+const { connect } = require('http2');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
@@ -37,10 +39,14 @@ const sessionConfig = {
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
-
-
 app.use(session(sessionConfig))
+app.use(flash());
 
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
 
 app.use('/campgrounds', campgrounds)
 app.use('/campgrounds/:id/reviews', reviews)
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -29,10 +29,12 @@ router.get('/new', (req, res) => {
 })
 
 router.post('/', validateCampground, catchAsync(async (req, res, next) => {
+
     // if (!req.body.campground) throw new ExpressError('Invalid Campground', 400);
 
     const campground = new Campground(req.body.campground);
     await campground.save();
+    req.flash('success', 'Successfully made a new campground')
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
