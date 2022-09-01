7e20851c7399a1ae87b6f71ff42b93b416c81953
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-09-02 01:21:21 +0530

    Configuring Passport

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ const session = require('express-session');
 const flash = require('connect-flash')
 const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override')
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user')
+
+
 
 const campgrounds = require('./routes/campgrounds')
 const reviews = require('./routes/reviews');
-const { connect } = require('http2');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
@@ -42,10 +46,23 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
+})
+
+app.get('/fakeUser', async (req, res) => {
+    const user = new User({ email: 'shantanu@gmail.com', username: 'shantanuuu' })
+    const newUser = await User.register(user, 'chicken');
+    res.send(newUser)
 })
 
 app.use('/campgrounds', campgrounds)
