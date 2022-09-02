c8f69d538eed619bbac5737df77d5ac1b4e9d451
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-09-03 00:17:12 +0530

    Adding an Author to Campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -34,13 +34,15 @@ router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, nex
     // if (!req.body.campground) throw new ExpressError('Invalid Campground', 400);
 
     const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
     await campground.save();
     req.flash('success', 'Successfully made a new campground')
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
 router.get('/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews');
+    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
+    console.log(campground);
     if (!campground) {
         req.flash('error', 'Cannot find that campground!')
         return res.redirect('/campgrounds')
--- seeds/index.js
+++ seeds/index.js
@@ -20,6 +20,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '63124c22a6bbf496c6ac1d7e',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
