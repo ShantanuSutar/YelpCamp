8767d613e4245a38d384f5afab6f0ae60db82fee
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-09-02 11:34:26 +0530

    Register Form

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ const LocalStrategy = require('passport-local');
 const User = require('./models/user')
 
 
-
-const campgrounds = require('./routes/campgrounds')
-const reviews = require('./routes/reviews');
+const userRoutes = require('./routes/users')
+const campgroundRoutes = require('./routes/campgrounds')
+const reviewRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
@@ -59,14 +59,9 @@ app.use((req, res, next) => {
     next();
 })
 
-app.get('/fakeUser', async (req, res) => {
-    const user = new User({ email: 'shantanu@gmail.com', username: 'shantanuuu' })
-    const newUser = await User.register(user, 'chicken');
-    res.send(newUser)
-})
-
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews', reviews)
+app.use('/', userRoutes)
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
 
 
 
