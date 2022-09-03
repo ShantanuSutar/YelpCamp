3b81e86633ba70471c4d7580a608405005cc81de
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-09-04 01:48:39 +0530

    Uploading To Cloudinary Basics

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const campgrounds = require('../controllers/campgrounds')
-const catchAsync = require('../utils/catchAsync')
-const { isLoggedIn, isAuthor, validateCampground } = require('../middleware')
-const multer = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const campgrounds = require('../controllers/campgrounds');
+const catchAsync = require('../utils/catchAsync');
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+const multer = require('multer');
+const { storage } = require('../cloudinary')
+const upload = multer({ storage });
 
 const Campground = require('../models/campground')
 
